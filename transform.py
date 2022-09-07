ee16b9b7eb495e062c04201edae58204357142d4
DatDieu <dat.dieu@hitachivantara.com>
2022-09-07 20:30:29 +0700

    add LDA

--- transform.py
+++ transform.py
@@ -24,19 +24,19 @@ class TransformerModel(tf.keras.Model):
         dec_output, attention_weights = self.decoder(tar, enc_output, training, look_ahead_mask, dec_padding_mask)
         
         final_output = self.final_layer(dec_output)
-        # full_topic = []
-        # a_topic = []
-        # a = []
-        # for e in inp.numpy():
-        #   a = 18*[0]
-        #   for el in e:
-        #     a = a + self.word2Topic[int(el)].numpy()
-        #   a_topic.append(a/300)
-        # full_topic.append(a_topic)
+        full_topic = []
+        a_topic = []
+        a = []
+        for e in inp.numpy():
+          a = 18*[0]
+          for el in e:
+            a = a + self.word2Topic[int(el)].numpy()
+          a_topic.append(a/300)
+        full_topic.append(a_topic)
        
-        # topic_arg1 = tf.matmul(tf.tile(tf.expand_dims(self.word2Topic, 0),[final_output.shape[0], 1, 1]), tf.cast(tf.reshape(full_topic,(final_output.shape[0],18,1)), dtype=tf.float32))
-        # topic_arg2 = tf.tile(tf.reshape(topic_arg1, (topic_arg1.shape[0], 1 , topic_arg1.shape[1])), [1,final_output.shape[1],1])
+        topic_arg1 = tf.matmul(tf.tile(tf.expand_dims(self.word2Topic, 0),[final_output.shape[0], 1, 1]), tf.cast(tf.reshape(full_topic,(final_output.shape[0],18,1)), dtype=tf.float32))
+        topic_arg2 = tf.tile(tf.reshape(topic_arg1, (topic_arg1.shape[0], 1 , topic_arg1.shape[1])), [1,final_output.shape[1],1])
         
-        # topic_arg3 =  tf.truediv(topic_arg2 , tf.reduce_max(topic_arg2))  
-        # sum_all = final_output * (1-alpha) + alpha*tf.cast(topic_arg3, dtype=tf.float32)
-        return final_output, enc_output, attention_weights
+        topic_arg3 =  tf.truediv(topic_arg2 , tf.reduce_max(topic_arg2))  
+        sum_all = final_output * (1-alpha) + alpha*tf.cast(topic_arg3, dtype=tf.float32)
+        return sum_all, enc_output, attention_weights
